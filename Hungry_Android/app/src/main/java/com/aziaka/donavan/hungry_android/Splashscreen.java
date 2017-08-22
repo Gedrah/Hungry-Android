41784944ec3f0cca7cf457f807eb4bd18b3e3af9
Donavan Aziaka <donavan.aziaka@epitech.eu>
2017-08-22 23:41:23 +0200

    added splashscreen (empty)

---
+++ Hungry_Android/app/src/main/java/com/aziaka/donavan/hungry_android/Splashscreen.java
@@ -0,0 +1,28 @@
+package com.aziaka.donavan.hungry_android;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private int SPLASH_SCREEN_TIMEOUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
